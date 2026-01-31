20bfaf2b264e6ec2db34cffeda433e991a2c2b8a
Efendi-Sugiantoro-16 <efendisugiantoro16@gmail.com>
2026-01-31 21:27:36 +0700

    feat: Add `create_badges_tables.php` to the list of migrations.

--- run_migrations.php
+++ run_migrations.php
@@ -6,6 +6,7 @@ $migrations = [
     'create_tables.php',
     'create_withdrawals_table.php',
     'create_notifications_table.php',
+    'create_badges_tables.php',
     'add_available_balance_to_users.php',
     'add_goal_id_to_withdrawals.php',
     'add_method_to_transactions.php',
